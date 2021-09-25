04260ab01772d7a2785acde0d04374057915cd79
André Marini <74572624+AndreVilson@users.noreply.github.com>
2021-09-25 16:06:22 -0300

    Add files via upload

---
+++ Controllers/AlunosController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AppMvc.Models;
+
+namespace AppMvc.Controllers
+{
+    [Authorize]
+    public class AlunosController : Controller
+    {
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("listar-Alunos")]
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Alunos.ToListAsync());
+        }
+
+        [HttpGet]
+        [Route("Aluno-detalhes/{id:int}")]
+        public async Task<ActionResult> Details(int id)
+        {
+         
+            var aluno = await db.Alunos.FindAsync(id);
+
+            if (aluno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aluno);
+        }
+
+        [HttpGet]
+        [Route("novo-aluno")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [Route("novo-aluno")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Nome,Email,CPF,Ativo")] Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                aluno.DataMatricula = DateTime.Now;
+                db.Alunos.Add(aluno);
+                await db.SaveChangesAsync();
+
+                TempData["Mensagem"] = "Aluno cadastrado com sucesso!";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(aluno);
+        }
+
+        [HttpGet]
+        [Route("editar-aluno/{id:int}")]
+        public async Task<ActionResult> Edit(int id)
+        {
+           
+            var aluno = await db.Alunos.FindAsync(id);
+            if (aluno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aluno);
+        }
+
+        [HttpPost]
+        [Route("editar-aluno/{id:int}")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Nome,Email,CPF,Ativo")] Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aluno).State = EntityState.Modified;
+                db.Entry(aluno).Property(a => a.DataMatricula).IsModified = false;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(aluno);
+        }
+
+        [HttpGet]
+        [Route("excluir-aluno/{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            
+            Aluno aluno = await db.Alunos.FindAsync(id);
+            if (aluno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aluno);
+        }
+
+
+        [HttpPost]
+        [Route("excluir-aluno/{id:int}")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Aluno aluno = await db.Alunos.FindAsync(id);
+            db.Alunos.Remove(aluno);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/Aluno.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AppMvc.Models
+{
+    public class Aluno
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        [DisplayName("Nome Completo")]
+        [RequiredAttribute(ErrorMessage = "O campo {0} é requirido")]
+        [MaxLength(100, ErrorMessage = "No máximo 100 caracteres")]
+        public string Nome { get; set; }
+
+        [DisplayName("E-mail")]
+        [RequiredAttribute(ErrorMessage = "O campo {0} é requirido")]
+        [MaxLength(100, ErrorMessage = "E-mail em formato inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é requerido")]
+        public string CPF { get; set; }
+
+        public DateTime DataMatricula { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
